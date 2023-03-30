c8f3065314e010daee3054dcd3f26603faac287c
EstefaniaR5 <estefania.rams29@gmail.com>
2023-03-30 00:48:46 -0600

    modificacion y correcion de archivo js y html

---
+++ filtro.js
@@ -0,0 +1,68 @@
+// Tenemos un li de productos
+const productos = [
+  {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
+  {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
+  {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
+  {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
+  {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
+]
+
+/* se cambio el getElementByName por elementById de lista productos
+y la constante $i por 'input' como la clase en la etiqueta input del html para
+que se mostraran los productos */
+const li = document.getElementById("lista-de-productos")
+const input = document.querySelector('.input');
+
+/* antes del for se agrega funcion de displayProductos que contiene la lista de los productos de la linea 2 */
+function displayProductos(productos){
+  for (let i = 0; i < productos.length; i++) {
+    var d = document.createElement("div")
+    d.classList.add("producto")
+  
+    var ti = document.createElement("p")
+    ti.classList.add("titulo")
+    ti.textContent = productos[i].nombre
+    
+    var imagen = document.createElement("img");
+    imagen.setAttribute('src', productos[i].img);
+  
+    d.appendChild(ti)
+    d.appendChild(imagen)
+  
+    li.appendChild(d)
+  }
+}
+displayProductos(productos)
+
+const botonDeFiltro = document.querySelector("button");
+
+botonDeFiltro.onclick = function() {
+  while (li.firstChild) {
+    li.removeChild(li.firstChild);
+  }
+
+  const texto = input.value;
+  console.log(texto);
+  const productosFiltrados = filtrado(productos, texto);
+
+  for (let i = 0; i < productosFiltrados.length; i++) {
+    var d = document.createElement("div")
+    d.classList.add("producto")
+  
+    var ti = document.createElement("p")
+    ti.classList.add("titulo")
+    ti.textContent = productosFiltrados[i].nombre
+    
+    var imagen = document.createElement("img");
+    imagen.setAttribute('src', productosFiltrados[i].img);
+  
+    d.appendChild(ti)
+    d.appendChild(imagen)
+  
+    li.appendChild(d)
+  }
+}
+
+const filtrado = (productos = [], texto) => {
+  return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
+}  
